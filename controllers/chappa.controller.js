06aa9f0ea5cbe0339aa65f38747ffaf14b9ac366
Natty Engeda <nattynengeda@gmail.com>
2023-01-13 15:08:52 +0300

    Test Commit

--- controllers/chappa.controller.js
+++ controllers/chappa.controller.js
@@ -66,7 +66,6 @@ export const verifyChapa = async (req, res) => {
       if (user.email_sent == 0) {
         console.log("Fetch Finished");
 
-        var strData = JSON.stringify(strData);
 
         qr.toFile(
           sentfile + "/" + user.confirmation_code + ".png",
@@ -140,92 +139,7 @@ export const verifyChapa = async (req, res) => {
     }
 
   });
-  // console.log(chapadata);
-  // if (chapadata.status == 'success') {
-  //   console.log("Chapa Data", chapadata);
-  //   var ts_ref = chapadata.tx_ref;
-  //   var event1 = await ActivityReserv.findAll({
-  //     where: {
-  //       tx_ref: ts_ref,
-  //     }
-  //   });
-
-  //   var event = event1[0];
-  //   console.log("event", event.email_sent);
-
-  //   if (event.email_sent == 0) {
-
-  //     // console.log("event", event);
-  //     console.log("Fetch Finished");
-
-  //     var strData = JSON.stringify(strData);
-
-  //     qr.toFile(
-  //       sentfile + "/" + event.confirmation_code + ".png",
-  //       "https://reservations.kurifturesorts.confirmation_com/login/" + event.confirmation_code,
-  //       async function (err, code) {
-  //         if (err) return res.json({ msg: "Error generating QR Code" });
-  //         console.log("QR Code generated");
-
-  //         var attachment;
-  //         const filepath = sentfile + "/" + event.confirmation_code + ".png";
-  //         const file = {
-  //           filename: "confirmation.jpg",
-  //           data: await fsPromises.readFile(filepath),
-  //         };
-  //         attachment = [file];
-
-  //         console.log("After QR Creation1");
-
-  //         // var qr_image = process.env.URL + '/qrimage/' + event.confirmation_code;
-  //         // Email that is to be sent
-  //         const emailSent = {
-  //           from: "Kuriftu Water Park <postmaster@reservations.kurifturesorts.com>",
-  //           to: event.email,
-  //           subject: "Kuriftu Resort",
-  //           attachment,
-  //           template: "kuriftu_design",
-  //           // template: "kuriftu_test",
-  //           "v:fname": event.first_name + " " + event.last_name,
-  //           // "v:location": location,
-  //           "v:email": event.email,
-  //           "v:quantity": event.quantity,
-  //           "v:reservation": 'Kuriftu WaterPark Reservation',
-  //           "v:reservationDate": dateFunction(event.reservation_date),
-  //           "v:confirmation": event.confirmation_code,
-  //           "v:price": event.price + " " + event.currency,
-  //           "v:payment": 'Chapa',
-  //           // "v:image": qr_image,
-  //           attachment
-  //         };
-
-  //         // Function that sends the email
-  //         client.messages
-  //           .create(DOMAIN, emailSent)
-  //           .then((res) => {
-  //             console.log(res);
-  //           })
-  //           .catch((err) => {
-  //             console.error(err);
-  //           });
-
-  //         await ActivityReserv.update({
-  //           payment_status: 'paid',
-  //           email_sent: true,
-  //         }, {
-  //           where: {
-  //             confirmation_code: event.confirmation_code
-  //           }
-  //         });
-  //         // res.json({ msg: 'succes' });
-  //       }
-  //     );
-  //   } else {
-  //     console.log("Email already sent");
-  //   }
-
-
-  // }
+ 
 };
 
 export const returnChappa = async (req, res) => {
--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,119 +1,14 @@
-// import request from "request";
-// import { Chapa } from "chapa-nodejs";
+import qr from "qrcode";
 
-// export const test = async (req, res) => {
-//   const chapa = new Chapa({
-//     secretKey: process.env.CHAPPA_API,
-//   });
-//   //  var request = require('request');
-//   const tx_ref = await chapa.generateTransactionReference({
-//     prefix: "TX", // defaults to `TX`
-//     size: 20, // defaults to `15`
-//   });
+export const test = async (req, res) => {
 
-//   var options = {
-//     method: "POST",
-//     url: "https://api.chapa.co/v1/transaction/initialize",
-//     headers: {
-//       Authorization: "Bearer CHASECK_TEST-2MBUcoLYAH4xPJZ8och3gYRLA4klhAg8",
-//     },
-//     formData: {
-//       amount: "200",
-//       currency: "ETB",
-//       email: "nattynengeda@gmail.com",
-//       first_name: "Natty",
-//       last_name: "Engeda",
-//       tx_ref: tx_ref,
-//       callback_url: "http://localhost:8000/verifyChapa",
-
-//     },
-//   };
-
-// axios.post('https://api.chapa.co/v1/transaction/initialize', {
-//   headers: {
-//     Authorization: "Bearer CHASECK_TEST-k1OznKI6893xmPpX6hCSWLU9uhn050Yp",
-//   },
-//   formData: {
-//     amount: "100",
-//     currency: "ETB",
-//     email: "abebe@bikila.com",
-//     first_name: "Abebe",
-//     last_name: "Bikila",
-//     tx_ref: tx_ref,
-//     callback_url: "https://chapa.co",
-
-//   },
-// }).then(res => )
-
-//   request(options, function async(error, response) {
-//     if (error) throw new Error(error);
-//     console.log(response.body);
-//     var full_response = JSON.parse(response.body);
-//     var check_out = full_response.data.checkout_url;
-//     // res.json(check_out);
-//     res.redirect(check_out);
-//   });
-// };
-
-// import { Headers } from "request";
-// import http from 'http';
-// import fetch from "node-fetch";
-// import Currency from "../models/Currency.model";
-// const axios = require('axios');
-// import axios, {isCancel, AxiosError} from 'axios';
-// import
-//  const fetch = require('node-fetch');
-// const axios = require('axios');\
-// const axios = require('axios')
-
-// export const test = async (req, res) => {
-//   const dateFunction = (datee) => {
-//     let ts = Date.now();
-//     let date_ob = new Date(datee);
-//     let date = date_ob.getDate();
-//     let month = date_ob.getMonth() + 1;
-//     let year = date_ob.getFullYear();
-
-//     var final = year + "-" + month + "-" + date;
-//     return final;
-//   };
-
-//   const checkETB = await Currency.findAll({
-//     limit: 1,
-//     order: [["updatedAt", "DESC"]],
-//   });
-//   var outdate = dateFunction(Date.now());
-//   var indate = dateFunction(checkETB[0].updatedAt);
-
-//   if (outdate === indate) {
-//     console.log("equal");
-//   } else {
-//     console.log("different");
-//   }
-// };
-
-// import qr from "qrcode";
-
-// const sentfile = "assets/images/qr_codes";
-
-
-// export const test = async (req, res) => {
-
-//   qr.toFile(
-//     sentfile + "/1234as1d5.png",
-//     "https://reservations.kurifturesorts.nfirmation_cocom/login/12345",
-//     function (err, code) {
-//       if (err) return res.json({ msg: "Error generating QR Code" });
-//       console.log("QR Pass");
-//     }
-//   );
-
-//   res.send("Hello");
-// }
-
-export const test = async(req, res) =>{
-  var nowdate = new Date();
-  nowdate.setMonth(nowdate.getMonth() + 2);
-
-  res.send(nowdate);
-}
+ const sentfile = process.env.QR_HOME;
+  qr.toFile(
+    sentfile + "/Hello.png",
+    "https://reservations.kurifturesorts.confirmation_com/login/" + "Hello",
+    async function (err, code) {
+      if (err) return res.json({ msg: "Error generating QR Code" });
+      console.log("QR Code generated");
+      res.send("QR Generated");
+    })
+};
