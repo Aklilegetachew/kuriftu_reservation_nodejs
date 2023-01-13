9e85623ff1643b465fcb9b7ee5393d21e7bf5b99
Natty Engeda <nattynengeda@gmail.com>
2023-01-13 15:10:24 +0300

    Error QR CODE push

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -7,7 +7,7 @@ export const test = async (req, res) => {
     sentfile + "/Hello.png",
     "https://reservations.kurifturesorts.confirmation_com/login/" + "Hello",
     async function (err, code) {
-      if (err) return res.json({ msg: "Error generating QR Code" });
+      if (err) return res.json({ msg: "Error generating QR Code", err: err });
       console.log("QR Code generated");
       res.send("QR Generated");
     })
