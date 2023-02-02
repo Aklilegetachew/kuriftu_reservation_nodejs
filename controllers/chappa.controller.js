d4299a9848993bbe87c43b3f7bbda121706dc871
samueladdisu <samueladdisu7@gmail.com>
2023-02-02 11:48:39 +0300

    expdate bug1

--- controllers/chappa.controller.js
+++ controllers/chappa.controller.js
@@ -123,8 +123,6 @@ export const verifyChapa = async (req, res) => {
 
             
 
-            console.log(exp);
-
             client.messages
                 .create(DOMAIN, emailSent)
                 .then((res) => {
