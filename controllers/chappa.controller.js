c4fe33d48f73cc063e7934439a34491ec568498e
samueladdisu <samueladdisu7@gmail.com>
2023-02-02 15:24:02 +0300

    chapa payload

--- controllers/chappa.controller.js
+++ controllers/chappa.controller.js
@@ -37,121 +37,121 @@ export const verifyChapa = async (req, res) => {
   const chapadata = req.body;
   const sentfile = process.env.QR_HOME;
 
-  console.log(chapaInData);
+  console.log(chapadata);
 
-  var options = {
-    'method': 'GET',
-    'url': 'https://api.chapa.co/v1/transaction/verify/' + chapaInData,
-    'headers': {
-      'Authorization': 'Bearer ' + process.env.CHAPA_API,
-    }
-  };
-  request(options, async function (error, response) {
-    if (error) throw new Error(error);
-    // console.log(response.body);
-    var verChapa = JSON.parse(response.body);
-    // console.log(verChapa);
-    console.log("Verification Finished");
-    if (verChapa.status === 'success') {
+  // var options = {
+  //   'method': 'GET',
+  //   'url': 'https://api.chapa.co/v1/transaction/verify/' + chapaInData,
+  //   'headers': {
+  //     'Authorization': 'Bearer ' + process.env.CHAPA_API,
+  //   }
+  // };
+  // request(options, async function (error, response) {
+  //   if (error) throw new Error(error);
+  //   // console.log(response.body);
+  //   var verChapa = JSON.parse(response.body);
+  //   // console.log(verChapa);
+  //   console.log("Verification Finished");
+  //   if (verChapa.status === 'success') {
 
-      var new_tx_ref = verChapa.data.tx_ref;
-      var user = await ActivityReserv.findAll({
-        where: {
-          tx_ref: new_tx_ref,
-        }
-      });
-      user = user[0];
-      console.log("USER", user);
+  //     var new_tx_ref = verChapa.data.tx_ref;
+  //     var user = await ActivityReserv.findAll({
+  //       where: {
+  //         tx_ref: new_tx_ref,
+  //       }
+  //     });
+  //     user = user[0];
+  //     console.log("USER", user);
 
-      if (user.email_sent == 0) {
-        console.log("Fetch Finished");
+  //     if (user.email_sent == 0) {
+  //       console.log("Fetch Finished");
 
 
-        qr.toFile(
-          sentfile + "/" + user.confirmation_code + ".png", user.confirmation_code,
-          async function (err, code) {
-            if (err) return res.json({ msg: "Error generating QR Code" });
-            console.log("QR Code generated");
+  //       qr.toFile(
+  //         sentfile + "/" + user.confirmation_code + ".png", user.confirmation_code,
+  //         async function (err, code) {
+  //           if (err) return res.json({ msg: "Error generating QR Code" });
+  //           console.log("QR Code generated");
 
-            var attachment;
-            const filepath = sentfile + "/" + user.confirmation_code + ".png";
-            const file = {
-              filename: "confirmation.jpg",
-              data: await fsPromises.readFile(filepath),
-            };
-            attachment = [file];
+  //           var attachment;
+  //           const filepath = sentfile + "/" + user.confirmation_code + ".png";
+  //           const file = {
+  //             filename: "confirmation.jpg",
+  //             data: await fsPromises.readFile(filepath),
+  //           };
+  //           attachment = [file];
 
-            console.log("After QR Creation");
+  //           console.log("After QR Creation");
 
-            // var qr_image = process.env.URL + '/qrimage/' + event.confirmation_code;
-            // Email that is to be sent
-            // var reservation_date = dateFunction(user)
-            var emailSent, subject, from, template;
-            if (user.location == 'waterpark') {
-              from = "Kuriftu Water Park"
-              subject = "You've successfully purchased Kuriftu Water Park ticket"
-              template = "waterpark"
+  //           // var qr_image = process.env.URL + '/qrimage/' + event.confirmation_code;
+  //           // Email that is to be sent
+  //           // var reservation_date = dateFunction(user)
+  //           var emailSent, subject, from, template;
+  //           if (user.location == 'waterpark') {
+  //             from = "Kuriftu Water Park"
+  //             subject = "You've successfully purchased Kuriftu Water Park ticket"
+  //             template = "waterpark"
               
   
-              // Function that sends the email
+  //             // Function that sends the email
               
-            } else if (user.location == 'entoto') {
-              from = "Kuriftu Resort and Spa Entoto"
-              subject = "Kuriftu Resort and Spa Entoto ticket Reservation"
-              template = 'entoto'
-            } else if (user.location == 'boston'){
-              from = "Boston Day Spa"
-              subject = "You've successfully purchased Boston Day Spa ticket"
-              template = "boston"
-            }
+  //           } else if (user.location == 'entoto') {
+  //             from = "Kuriftu Resort and Spa Entoto"
+  //             subject = "Kuriftu Resort and Spa Entoto ticket Reservation"
+  //             template = 'entoto'
+  //           } else if (user.location == 'boston'){
+  //             from = "Boston Day Spa"
+  //             subject = "You've successfully purchased Boston Day Spa ticket"
+  //             template = "boston"
+  //           }
 
-            emailSent = {
-              from: from + "<no-reply@reservations.kurifturesorts.com>",
-              to: user.email,
-              subject,
-              template,
-              "v:firstname": user.first_name,
-              "v:name": user.first_name + " " + user.last_name,
-              "v:email": user.email,
-              "v:confirmation": user.confirmation_code,
-              "v:quantity": user.quantity,
-              "v:price": user.price + " " + user.currency,
-              "v:purchaseDate": dateFunction(user.createdAt),
-              "v:expDate": dateFunction(user.createdAt.setMonth(user.createdAt.getMonth() + 3),),
-              attachment
-            };
+  //           emailSent = {
+  //             from: from + "<no-reply@reservations.kurifturesorts.com>",
+  //             to: user.email,
+  //             subject,
+  //             template,
+  //             "v:firstname": user.first_name,
+  //             "v:name": user.first_name + " " + user.last_name,
+  //             "v:email": user.email,
+  //             "v:confirmation": user.confirmation_code,
+  //             "v:quantity": user.quantity,
+  //             "v:price": user.price + " " + user.currency,
+  //             "v:purchaseDate": dateFunction(user.createdAt),
+  //             "v:expDate": dateFunction(user.createdAt.setMonth(user.createdAt.getMonth() + 3),),
+  //             attachment
+  //           };
 
             
 
-            client.messages
-                .create(DOMAIN, emailSent)
-                .then((res) => {
-                  console.log(res);
-                })
-                .catch((err) => {
-                  console.error(err);
-                });
-            console.log("Email Sent");
-            await ActivityReserv.update({
-              payment_status: 'paid',
-              email_sent: true,
-            }, {
-              where: {
-                confirmation_code: user.confirmation_code
-              }
-            });
-            res.sendStatus(200);
-          }
-        );
-      } else {
-        console.log("Email Already Sent");
-        res.sendStatus(200);
-      }
-    } else {
-      console.log("Chapa Verification Failed");
-    }
+  //           client.messages
+  //               .create(DOMAIN, emailSent)
+  //               .then((res) => {
+  //                 console.log(res);
+  //               })
+  //               .catch((err) => {
+  //                 console.error(err);
+  //               });
+  //           console.log("Email Sent");
+  //           await ActivityReserv.update({
+  //             payment_status: 'paid',
+  //             email_sent: true,
+  //           }, {
+  //             where: {
+  //               confirmation_code: user.confirmation_code
+  //             }
+  //           });
+  //           res.sendStatus(200);
+  //         }
+  //       );
+  //     } else {
+  //       console.log("Email Already Sent");
+  //       res.sendStatus(200);
+  //     }
+  //   } else {
+  //     console.log("Chapa Verification Failed");
+  //   }
 
-  });
+  // });
 
 };
 
