ca15b119e6203df6e843cab9feba7dcef894d513
Aklilegetachew <aklilegetachew@gmail.com>
2023-02-27 14:32:08 +0300

    redeemd tickets only

--- controllers/Activity.controller.js
+++ controllers/Activity.controller.js
@@ -29,8 +29,7 @@ export const add_activity_price = async (req, res) => {
 };
 
 export const view_activity_reservation = async (req, res) => {
-
-  const location = req.body.location
+  const location = req.body.location;
   try {
     if (location == "all") {
       const result = await ActivityReserv.findAll();
@@ -42,13 +41,13 @@ export const view_activity_reservation = async (req, res) => {
       //     console.error(`Error parsing JSON data: ${error.message}`);
       //   }
       // })
-  
+
       res.json(result);
     } else {
       const result = await ActivityReserv.findAll({
         where: {
-          location: location
-        }
+          location: location,
+        },
       });
       result.forEach((item) => {
         try {
@@ -57,13 +56,82 @@ export const view_activity_reservation = async (req, res) => {
         } catch (error) {
           console.error(`Error parsing JSON data: ${error.message}`);
         }
-      })
+      });
       console.log(result);
       res.json(result);
     }
-  
   } catch (error) {
     console.log(error);
   }
-  
+};
+
+export const view_redemed_reservation = async (req, res) => {
+  const location = req.body.location;
+  try {
+    if (location == "all") {
+      const result = await ActivityReserv.findAll({
+        where: {
+          order_status: "checked_in",
+        },
+      });
+     
+      res.json(result);
+    } else {
+      const result = await ActivityReserv.findAll({
+        where: {
+          location: location,
+          order_status: "checked_in",
+        },
+      });
+      result.forEach((item) => {
+        try {
+          item.amt = JSON.parse(item.amt);
+          item.redeemed_amt = JSON.parse(item.redeemed_amt);
+        } catch (error) {
+          console.error(`Error parsing JSON data: ${error.message}`);
+        }
+      });
+      console.log(result);
+      res.json(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const view_unpaid_activity = async (req, res) => {
+  const status = req.body.paymentStatus;
+  try {
+    if (location == "all") {
+      const result = await ActivityReserv.findAll();
+      // result.forEach((item) => {
+      //   try {
+      //     item.amt = JSON.parse(item.amt);
+      //     item.redeemed_amt = JSON.parse(item.redeemed_amt);
+      //   } catch (error) {
+      //     console.error(`Error parsing JSON data: ${error.message}`);
+      //   }
+      // })
+
+      res.json(result);
+    } else {
+      const result = await ActivityReserv.findAll({
+        where: {
+          payment_status: status,
+        },
+      });
+      result.forEach((item) => {
+        try {
+          item.amt = JSON.parse(item.amt);
+          item.redeemed_amt = JSON.parse(item.redeemed_amt);
+        } catch (error) {
+          console.error(`Error parsing JSON data: ${error.message}`);
+        }
+      });
+      console.log(result);
+      res.json(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
--- routes/router.js
+++ routes/router.js
@@ -1,42 +1,46 @@
-import express from "express"
+import express from "express";
 import {
   add_activity_price,
   view_activity_price,
   view_activity_reservation,
-} from "../controllers/Activity.controller"
-import { auth } from "../controllers/authentication.controller"
-import {  verifyChapa } from "../controllers/chappa.controller"
-import { main } from "../controllers/index.controller"
-import { qrimage } from "../controllers/qrimages.controllers"
-import { acceptRequest } from "../controllers/request.controller"
-import { test } from "../controllers/test.controller"
+  view_unpaid_activity,
+  view_redemed_reservation,
+} from "../controllers/Activity.controller";
+import { auth } from "../controllers/authentication.controller";
+import { verifyChapa } from "../controllers/chappa.controller";
+import { main } from "../controllers/index.controller";
+import { qrimage } from "../controllers/qrimages.controllers";
+import { acceptRequest } from "../controllers/request.controller";
+import { test } from "../controllers/test.controller";
 import {
   checkGuest,
   verify,
   checkEntotoGuest,
-  checkBostonGuest
-} from "../controllers/verification.controller"
+  checkBostonGuest,
+} from "../controllers/verification.controller";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", main)
-router.get("/index", main)
-router.post("/request", acceptRequest)
-router.get("/test", test)
-router.get("/qrimage/:id", qrimage)
-router.post("/verify", verify)
-router.post("/checkGuest", checkGuest)
-router.get("/auth/:id", auth)
+router.get("/", main);
+router.get("/index", main);
+router.post("/request", acceptRequest);
+router.get("/test", test);
+router.get("/qrimage/:id", qrimage);
+router.post("/verify", verify);
+router.post("/checkGuest", checkGuest);
+router.get("/auth/:id", auth);
 
 // Activity
-router.get("/view_activity_price", view_activity_price)
-router.post("/add_activity_price", add_activity_price)
-router.post("/view_activity_reservation", view_activity_reservation)
+router.get("/view_activity_price", view_activity_price);
+router.post("/add_activity_price", add_activity_price);
+router.post("/view_activity_reservation", view_activity_reservation);
+router.post("/view_redemed_reservation", view_redemed_reservation);
+router.post("/view_activity_unpaid", view_unpaid_activity);
 // Chappa Payment
-router.post("/verifyChapa", verifyChapa)
-router.post("/checkEntotoGuest", checkEntotoGuest)
-router.post("/checkBostonGuest", checkBostonGuest)
+router.post("/verifyChapa", verifyChapa);
+router.post("/checkEntotoGuest", checkEntotoGuest);
+router.post("/checkBostonGuest", checkBostonGuest);
 
 // router.post('/verifyWebhook',);
 
-export default router
+export default router;
