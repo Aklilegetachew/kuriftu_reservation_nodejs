bb830949c82e28ace1c27cf15c9fa6e69ad8335a
samueladdisu <samueladdisu7@gmail.com>
2023-05-12 08:51:05 +0000

    from VPS

--- controllers/qrimages.controllers.js
+++ controllers/qrimages.controllers.js
@@ -1,31 +1,34 @@
 import path from "path";
 import fs from "fs";
+import dotenv, { config } from "dotenv";
+
+dotenv.config();
 
 export const qrimage = async (req, res) => {
-//   var id = req.params.id;
-// //   console.log(id);
-//   try {
-//   var dir = path.join('assets/images/qr_codes' + "/" + id + ".png");
+  const sentfile = process.env.QR_HOME;
 
-//   var mime = {
-//     html: "text/html",
-//     txt: "text/plain",
-//     css: "text/css",
-//     gif: "image/gif",
-//     jpg: "image/jpeg",
-//     png: "image/png",
-//     svg: "image/svg+xml",
-//     js: "application/javascript",
-//   };
+  var id = req.params.id;
+  console.log(id);
+  try {
+    var dir = path.join(sentfile + "/" + id + ".png");
+    // var dir = path.join(__dirname + "/image.jpg");
 
-    
-  
-//     res.writeHead(200, { "Content-type": "image/jpeg" });
-//     fs.createReadStream(dir).pipe(res);
-//   } catch (error) {
-//     console.log(error);
-//   }
+    var mime = {
+      html: "text/html",
+      txt: "text/plain",
+      css: "text/css",
+      gif: "image/gif",
+      jpg: "image/jpeg",
+      png: "image/png",
+      svg: "image/svg+xml",
+      js: "application/javascript",
+    };
 
-//   res.send(dir);
-console.log('Hello');
+    res.writeHead(200, { "Content-type": "image/jpeg" });
+    fs.createReadStream(dir).pipe(res);
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+  //   res.send(dir);
 };
